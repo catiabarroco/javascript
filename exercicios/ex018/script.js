f79d37d4ca04ced0efc93f1dba08176b1c6c93b9
catiabarroco <94981612+catiabarroco@users.noreply.github.com>
2021-12-03 10:02:15 +0000

    exercicio while/for

--- exercicios/ex018/script.js
+++ exercicios/ex018/script.js
@@ -1,19 +1,46 @@
-function calcular(){
-    var inicio = Number(window.document.getElementById('txtin').value)
-    var fim =  Number(window.document.getElementById('txtfim').value )
-    var passo = Number(window.document.getElementById('txtpass').value) 
+function calcular() {
+    var inicio = window.document.getElementById('txtin')
+    var fim =  window.document.getElementById('txtfim')
+    var passo = window.document.getElementById('txtpass') 
     var res = window.document.getElementById('res')
 
-    var c = inicio
-    while (c<=fim){
-        res.innerHTML += (`${c} 👉`)
-        c = c + passo
+    if (inicio.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
+        //window.alert('[ERRO] Faltam valores')
+        res.innerHTML = 'Impossivel Contar'
+    } else {
+        res.innerHTML = 'Contando:...<br>'
+        var i = Number(inicio.value)
+        var f = Number(fim.value)
+        var p = Number(passo.value)
+
+        if (p<=0) {
+            window.alert('Passo inválido Considerando passo [1]')
+            p = 1
+        }
+
+        if (i<f) {
+            //contagem crescente
+            for (var c = i; c <= f; c += p) {
+                res.innerHTML += (`${c} \u{1F449}`)
+            }
+        }else {
+            //contagem regressiva
+            for (var c = i; c >= f; c -= p) {
+                res.innerHTML += (`${c} \u{1F449}`)
+            }
+            
+        }
+        res.innerHTML += `\u{1F3C1}`
     }
-
-
-/*
-    for (var c =1 ; c<=10 ; c++) {
-        res.innerHTML = (`Teste ${c}`)
-    }*/
 }
+
+    
+/*
+        var c = i
+        while (c<=f){
+            res.innerHTML += (`${c} 👉`)
+            c = c + pas */
+
+
+
 
